853ee4124622ad559dcf83d1637c61075602941d
José Gabriel da Silva Lima <silva.jose@academico.ifpb.edu.br>
2021-05-23 16:39:07 -0300

    Implementação do Single-Table (relação de herança com JPA), e atuallização da classe EnderecoRepository (pois a mesma estava dando um erro)

--- com.bookstore/src/main/java/com/bookstore/com/bookstore/repository/EnderecoRepository.java
+++ com.bookstore/src/main/java/com/bookstore/com/bookstore/repository/EnderecoRepository.java
@@ -8,6 +8,5 @@ import com.bookstore.com.bookstore.model.Endereco;
 
 public interface EnderecoRepository extends JpaRepository<Endereco, Long>{
 
-	public List<com.bookstore.com.bookstore.model.Endereco> findByNome(String nome);
-	
+//	public List<com.bookstore.com.bookstore.model.Endereco> findByNome(String nome); 
 }
