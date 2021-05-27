4c95f9f5554b137ab45c2e11d717e33298c3ce70
José Gabriel da Silva Lima <silva.jose@academico.ifpb.edu.br>
2021-05-26 23:49:05 -0300

    Criação do ItemPedidoRepository e atualização da classe AutorRepository

--- com.bookstore/src/main/java/com/bookstore/com/bookstore/repository/AutorRepository.java
+++ com.bookstore/src/main/java/com/bookstore/com/bookstore/repository/AutorRepository.java
@@ -1,5 +1,7 @@
 package com.bookstore.com.bookstore.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.bookstore.com.bookstore.model.Autor;
@@ -7,4 +9,5 @@ import com.bookstore.com.bookstore.model.Autor;
 @Repository
 public interface AutorRepository extends JpaRepository<Autor, Long>{
 
+	public List<Autor> findByNome(String nome);
 }
