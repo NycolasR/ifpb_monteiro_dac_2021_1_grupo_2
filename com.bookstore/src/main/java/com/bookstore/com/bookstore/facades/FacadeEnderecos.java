5135dfae6ad35708506b7198d6821d4a1e7f0f27
PedroSCY <Hock.gonzagas7@gmail.com>
2021-06-02 19:06:45 -0300

    Documentação

--- com.bookstore/src/main/java/com/bookstore/com/bookstore/facades/FacadeEnderecos.java
+++ com.bookstore/src/main/java/com/bookstore/com/bookstore/facades/FacadeEnderecos.java
@@ -6,12 +6,31 @@ import org.springframework.stereotype.Component;
 import com.bookstore.com.bookstore.model.Endereco;
 import com.bookstore.com.bookstore.service.EnderecoService;
 
+/**
+ * 
+ * @author NPG (nome dado a equipe que esta desenvolvendo esse sistema)
+ * Classe Facade responsável por métodos relacionados a Endereco. Esse padrão de projeto
+ * foi escolhido para ser implementado, pois o mesmo busca facilitar a utilização de 
+ * métodos de classes distintas que se relacionam entre si, por parte das classes clientes.
+ *
+ */
 @Component
 public class FacadeEnderecos {
 
 	@Autowired
 	private EnderecoService enderecoService;
 	
+	/**
+	 * Esse metodo é ultilizado para facilitar a criação de um objeto(Endereco).
+	 * @param rua Nome da rua 
+	 * @param numero Numero da residencia
+	 * @param bairro Bairro da residencia
+	 * @param uf Sigla do Estado 
+	 * @param cidade Nome da cidade
+	 * @param complemento Possiveis complementos
+	 * @param cep CEP da cidade
+	 * @return Retorna um objeto(Endereco)
+	 */
 	public Endereco criarEndereco(
 			String rua, 
 			Integer numero, 
--- com.bookstore/src/main/java/com/bookstore/com/bookstore/repository/UsuarioRepository.java
+++ com.bookstore/src/main/java/com/bookstore/com/bookstore/repository/UsuarioRepository.java
@@ -7,11 +7,26 @@ import org.springframework.stereotype.Repository;
 
 import com.bookstore.com.bookstore.model.Usuario;
 
+/**
+ * 
+ * @author NPG (nome dado a equipe que esta desenvolvendo esse sistema)
+ *
+ */
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
+	/**
+	 * Metodo que busca todos os usuarios cadastrados com o nome passado por parametro.
+	 * @param nome Nome que deseja-se pesquisar.
+	 * @return Uma lista com os usuarios encontrados.
+	 */
 	public List<Usuario> findByNome(String nome);
 	
+	/**
+	 * Metodo que busca todos os usuario cadastrados pelo email passado por parametro.
+	 * @param email Email que deseja-se pesquisar.
+	 * @return Uma lista com os usuarios encontrados.
+	 */
 	public List<Usuario> findByEmailIs(String email);
 	
 }
