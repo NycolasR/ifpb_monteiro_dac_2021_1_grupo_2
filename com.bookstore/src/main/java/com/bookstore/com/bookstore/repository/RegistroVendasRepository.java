fc4f00edcbda9f9df0c308f87ad3ce2ce5afdfeb
NycolasR <nycolasramon3@gmail.com>
2021-06-02 19:01:39 -0300

    Comentários de javadoc e normais adicionados.

--- com.bookstore/src/main/java/com/bookstore/com/bookstore/repository/RegistroVendasRepository.java
+++ com.bookstore/src/main/java/com/bookstore/com/bookstore/repository/RegistroVendasRepository.java
@@ -5,6 +5,14 @@ import org.springframework.stereotype.Repository;
 
 import com.bookstore.com.bookstore.model.RegistroVendas;
 
+/**
+ * 
+ * @author NPG (Grupo 2)
+ * Interface para acesso ao banco de dados voltado para a entidade RegistroVendas.
+ * Implementações fornecidas pelo Spring, logo não é necessário a uma classe
+ * implementar esta interface.
+ * 
+ */
 @Repository
 public interface RegistroVendasRepository extends JpaRepository<RegistroVendas, Long> {
 	
