01f4f6c526c83268077138d68c8cd3ba8e4ec980
José Gabriel da Silva Lima <silva.jose@academico.ifpb.edu.br>
2021-07-12 15:13:12 -0300

    criação de uma tela de administração e seu controller. Listagem dos autores no htlm vom thymeleaf

--- com.bookstore/src/main/java/com/bookstore/controller/ControllerCrudAutores.java
+++ com.bookstore/src/main/java/com/bookstore/controller/ControllerCrudAutores.java
@@ -1,15 +1,28 @@
 package com.bookstore.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.bookstore.model.Autor;
+import com.bookstore.service.AutorService;
 
 @Controller
 public class ControllerCrudAutores {
 
+	@Autowired
+	private AutorService autorService;
+
 	@GetMapping("/autor")
-	public String recuperarAutores() {
-		
+	public String recuperarAutores(Model model) {
+
+		List<Autor> autores = autorService.recuperarAutores();
+		model.addAttribute("autores", autores);
+
 		return "autores/autores";
 	}
-	
+
 }
