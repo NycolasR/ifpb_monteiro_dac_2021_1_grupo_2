def117ee361b6133b4911a82ecd5e4216f34edd2
José Gabriel da Silva Lima <silva.jose@academico.ifpb.edu.br>
2021-07-16 18:48:58 -0300

    atualização da página de autores, controller de autores e criação da tela crud de autores

--- com.bookstore/src/main/java/com/bookstore/controller/ControllerCrudAutores.java
+++ com.bookstore/src/main/java/com/bookstore/controller/ControllerCrudAutores.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.bookstore.facades.FacadeAutor;
 import com.bookstore.model.Autor;
 import com.bookstore.service.AutorService;
 
@@ -15,6 +19,9 @@ public class ControllerCrudAutores {
 
 	@Autowired
 	private AutorService autorService;
+	
+	@Autowired
+	private FacadeAutor facadeAutor;
 
 	@GetMapping("/autor")
 	public String recuperarAutores(Model model) {
@@ -24,6 +31,60 @@ public class ControllerCrudAutores {
 		model.addAttribute("autores", autores);
 		
 		return "autores/autores";
+	}
+	
+	@GetMapping("/autorform/{id}")
+	public String resgatarFormulario(@PathVariable("id") Long id, Model model) {
+		
+		Autor autor = new Autor("");
+		
+		if(id > 0) {
+			 
+			 autor = autorService.recuperarPeloId(id).get();
+		}
+		
+		model.addAttribute("autor",autor);
+		
+		return "autores/autoresform";
+	}
+	
+	@PostMapping("/autorformadd")
+	public String criarAutor(@ModelAttribute Autor autor) {
+		
+		try {
+			facadeAutor.criarAutor(autor.getNome());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return "redirect:/autor";
+	}
+	
+	@PostMapping("/autorformupdate/{id}")
+	public String atualizarAutor(@PathVariable("id") Long id) {
+		
+		try {
+			facadeAutor.atualizarAutor(facadeAutor.recuperarAutor(id));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return "redirect:/autor";
+	}
+	
+	@PostMapping("/autorformremove/{id}")
+	public String removerAutor(@PathVariable("id") Long id) {
+		
+		try {
+			facadeAutor.removerAutor(id);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return "redirect:/autor";
 	}
 
 }
