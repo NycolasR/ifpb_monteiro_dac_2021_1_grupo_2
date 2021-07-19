3111a7586387962346fc37485fb7cc1c6845a12e
Nycolas R. Alves <57738113+NycolasR@users.noreply.github.com>
2021-07-19 00:41:52 -0300

    Estilização melhorada + acesso ao bd nas duas primeiras abas

--- com.bookstore/src/main/java/com/bookstore/controller/ControllerUsuario.java
+++ com.bookstore/src/main/java/com/bookstore/controller/ControllerUsuario.java
@@ -1,11 +1,14 @@
 package com.bookstore.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.bookstore.model.Endereco;
 import com.bookstore.model.Usuario;
 import com.bookstore.service.UsuarioService;
 
@@ -18,12 +21,30 @@ public class ControllerUsuario {
 	@GetMapping("/perfil/{id}")
 	public String recuperarPaginaPerfil(@PathVariable("id") Long id, Model model) {
 	
-//			Usuario usuar = sriorviecuperPeloId(id);
-//			model.addAttriffsuar, uriff
-//		} catch (Ex
-//
+		Usuario usuario;
+		List<Endereco> enderecos;
 		
+		try {
+			usuario = usuarioService.recuperarPeloId(id);
+			enderecos = usuario.getEnderecos();
+			
+			model.addAttribute("usuario", usuario);
+			model.addAttribute("enderecos", enderecos);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return "user/profile";
 	}
-
+	
+	
+	/*
+	 * oefkf
+	 * dsafD
+	 *
+	 * 
+	 *
+	 * 
+	 * 
+	 */
 }
