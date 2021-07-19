c6629954bc656b078cc31c0a0364f36476572182
José Gabriel da Silva Lima <silva.jose@academico.ifpb.edu.br>
2021-07-19 02:02:31 -0300

    Atualização de Controller, Facade, Service, Repository, html e form de Editora

--- com.bookstore/src/main/java/com/bookstore/controller/ControllerCrudEditoras.java
+++ com.bookstore/src/main/java/com/bookstore/controller/ControllerCrudEditoras.java
@@ -1,14 +1,85 @@
 package com.bookstore.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.bookstore.facades.FacadeEditoras;
+import com.bookstore.model.Editora;
 
 @Controller
 public class ControllerCrudEditoras {
 
+	@Autowired
+	private FacadeEditoras facadeEditoras;
+
 	@GetMapping("/editora")
-	public String recuperarEditoras() {
-		
+	public String recuperarEditoras(Model model) {
+
+		model.addAttribute("editoras", facadeEditoras.recuperarEditoras());
+
 		return "editoras/editoras";
 	}
+
+	@GetMapping("/editoraform/{id}")
+	public String resgatarFormulario(@PathVariable("id") Long id, Model model) {
+
+		try {
+
+			if (id > 0) {// os ids cadastrados no banco são maiores que 0
+
+				model.addAttribute("editora", facadeEditoras.recuperarEditora(id));
+
+			} else {
+
+				model.addAttribute("editora", facadeEditoras.recuperarEditoraNula());
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return "editoras/editorasform";
+	}
+
+	@PostMapping("/editoraformadd")
+	public String criarEditora(@ModelAttribute Editora editora) {
+
+		try {
+			facadeEditoras.criarEditora(editora);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return "redirect:/editora";
+	}
+
+	@PostMapping("/editoraformupdate/{id}")
+	public String atualizarEditora(@ModelAttribute Editora editoraDto, @PathVariable("id") Long id) {
+
+		try {
+			facadeEditoras.atualizarEditora(editoraDto, id);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return "redirect:/editora";
+	}
+
+	@PostMapping("/editoraformremove/{id}")
+	public String removerEditora(@PathVariable("id") Long id) {
+
+		try {
+			facadeEditoras.removerEditora(id);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return "redirect:/editora";
+	}
+
 }
