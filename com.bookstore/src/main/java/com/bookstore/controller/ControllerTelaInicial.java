5c58751330650fe579ad9b77a2703500ddf42343
José Gabriel da Silva Lima <silva.jose@academico.ifpb.edu.br>
2021-08-06 01:18:00 -0300

    Atualização dos controllers de Licro e da tela inicial

--- com.bookstore/src/main/java/com/bookstore/controller/ControllerTelaInicial.java
+++ com.bookstore/src/main/java/com/bookstore/controller/ControllerTelaInicial.java
@@ -1,5 +1,6 @@
 package com.bookstore.controller;
 
+import java.lang.ProcessBuilder.Redirect;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -24,52 +24,71 @@ public class ControllerTelaInicial {
 
 	@Autowired
 	private FacadeLivros facadeLivros;
-	
+
 	@Autowired
 	private FacadeCategoria facadeCategoria;
-	
+
 	private Integer pagina = 1;
-	
+	private List<Integer> categorias = null;
+	private String stringDeBusca = "";
+
 	@GetMapping("/inicio")
 	public String recuperarTelaInicial(Model model, @AuthenticationPrincipal Usuario usuario) {
-		
+
 		model.addAttribute("usuario", usuario);
+
+		System.out.println(stringDeBusca);
 		
 		try {
 			
-			Page<Livro> livros = facadeLivros.paginarLivros("titulo", Direction.ASC, pagina, false);
-			
-			System.out.println( facadeCategoria.recuperarCategorias());
-			
+			Page<Livro> livros = facadeLivros.paginarLivros("titulo", Direction.ASC, pagina, false, categorias, stringDeBusca);
+
+			System.out.println(facadeCategoria.recuperarCategorias());
+
 			model.addAttribute("categorias", facadeCategoria.recuperarCategorias());
 			model.addAttribute("livros", livros);
 			model.addAttribute("numeracao", facadeLivros.criarPaginacao(livros.getTotalPages(), pagina));
 			model.addAttribute("fim", livros.getTotalPages());
 			
 		} catch (Exception e) {
+			stringDeBusca = "";
 			e.printStackTrace();
+			return "redirect:/inicio";
 		}
-		
 
+		stringDeBusca = "";
+		
 		return "telasiniciais/tela-inicial";
 	}
-	
+
 	@GetMapping("/ecolher_pagina_inicio/{id}")
 	public String escolherPagina(@PathVariable Integer id) {
-		
+
 		pagina = id;
-		
+
 		return "redirect:/inicio";
 	}
-	
+
 	@GetMapping("/buscar_categoria")
-	public String buscarPorCategoria(@RequestParam("categorias") List<String> categorias) {
+	public String buscarPorCategoria(@RequestParam("categorias") List<Integer> categorias) {
+
+		this.categorias = null;
 		
-		for(int i = 0; i < categorias.size(); i++) {
-			System.out.println("Categoria: "+categorias.get(i));
+		if (categorias.size() > 1) {
+			categorias.remove(0);
+			this.categorias = categorias;
 		}
-		
-//		System.out.println("Categoria: "+categoria);
+		pagina = 1;
+
 		return "redirect:/inicio";
 	}
+
+	@GetMapping("/buscar_nome")
+	public String buscarPorNome(@RequestParam("nome_livro") String nomeLivro) {
+
+		stringDeBusca = nomeLivro;
+
+		return "redirect:/inicio";
+	}
+
 }
